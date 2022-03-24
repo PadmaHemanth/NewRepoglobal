8e529998120ca0a65f6085022df9d5a4c8742548
padma <padma@DESKTOP-FM8OD3M>
2022-03-24 23:54:38 +0530

    metro 1st trail

---
+++ metro/src/main/java/sampleDemo/Demo.java
@@ -0,0 +1,12 @@
+package sampleDemo;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	
+	@Test
+	public void createMetro() {
+		System.out.println("welcome to vijaynagar metro station");
+	}
+
+}
